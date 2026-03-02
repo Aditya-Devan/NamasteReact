fd51783ccccd263de04ad43da61ad02b47df36b0
Aditya-Devan <iamadyadevan@gmail.com>
2026-03-02 23:10:37 +0530

    Done with functonal components and build a solid foundation

--- App.js
+++ App.js
@@ -1,8 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading=React.createElement("h1",{},"Hellow World From React");
+ //functional react components
+const Title=()=>(
+    <h1 className="Heading" tabIndex={5} >
+        Namaste React Using JSX
+    </h1>
+);
+
+const HeadingComponent=()=>(
+  <div id="container">
+    {Title()}
+    <Title />
+    <Title></Title>
+    <h1 className="heading">Namaste React Functional Component Working</h1>
+  </div>
+);
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading); 
+root.render(<HeadingComponent/>);
+
+
