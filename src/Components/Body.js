2574fdde20bb1aeebc5b7f11fe0abdd306295450
Aditya-Devan <iamadyadevan@gmail.com>
2026-03-20 14:54:16 +0530

    Independent of mock data , now we've fetched data from swiggy's offcial website via api call and also learnt about cors

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,33 +1,26 @@
-// import RestaurantCard from "./RestaurantCard";
-// import { useState } from "react";
-// import { resList } from "../utils/mockData";
-
-// const Body = () => {
-//   return (
-//     <div className="body">
-//       <div className="search">Search</div>
-
-//       <div className="res-contain">
-//         {resList.map((restaurant) => (
-//           <RestaurantCard
-//             key={restaurant.info.id}
-//             resData={restaurant}
-//           />
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Body;
-
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
+import { useState , useEffect } from "react";
 import { resList } from "../utils/mockData";
 
 const Body = () => {
   // Local State Variable - Super powerful variable
   const [listOfRestaurants, setListOfRestraunt] = useState(resList);
+
+  useEffect(()=>{
+    fetchData();
+  },[]);
+
+  const fetchData= async()=>{
+     const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.6232624&lng=73.7417281&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+  
+        const json=await data.json();
+        console.log(json);
+        setListOfRestraunt(
+  json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+);
+
+    }
+
 
   return (
     <div className="body">
