815d5bbd965b56fc77024a59b8818df1a9cacb8a
Aditya-Devan <iamadyadevan@gmail.com>
2026-03-21 16:37:14 +0530

    Build the search functionality in the app. Used hooks effectively for that .

--- src/Components/RestaurantCard.js
+++ src/Components/RestaurantCard.js
@@ -18,10 +18,10 @@ const RestaurantCard = (props) => {
       <h3>{resData.info.name}</h3>
       <h4>{resData.info.cuisines.join(", ")}</h4>
       <h4>{resData.info.avgRating} stars</h4>
-      <h4>{resData.info.costForTwo / 100} FOR TWO</h4>
+      <h4>{resData.info.costForTwo} </h4>
       <h4>{resData.info.sla.deliveryTime} minutes</h4>
     </div>
   );
 };
 
-export default RestaurantCard;
+export default RestaurantCard;     
