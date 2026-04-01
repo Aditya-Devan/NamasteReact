10115ba0ef18bd45f7366aca983c77814296438e
Aditya-Devan <iamadyadevan@gmail.com>
2026-04-01 17:51:11 +0530

    Learn about the class components along with declaration , syntax, passing and recieving props and upadation and creation of state variables in cklass components

---
+++ src/Components/userClass.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+class UserClass extends React.Component{
+      constructor(props){
+        super(props);
+        this.state={
+            count:0,
+        }
+      }
+    render(){
+        return(
+     <div>
+      <h1>Count:{this.state.count}</h1>
+      <button onClick={()=>{
+         this.setState({
+            count:this.state.count+1,
+         })
+      }} >Increase Count</button>
+      <h2>Name:{this.props.Name}</h2>
+      <h3>Location:{this.props.location}</h3>
+
+     </div>
+        )
+    
+    }
+}
+
+export default UserClass;
---
+++ src/Components/userFxnl.jsx
@@ -0,0 +1,11 @@
+const UserFxnl=(props)=>{
+   return(
+    <div>
+      <h2>Name:{props.Name}</h2>
+      <h3>Location:{props.location}</h3>
+
+    </div>
+   );
+}
+
+export default UserFxnl;
